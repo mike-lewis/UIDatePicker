079bebf7ff347a05b8b12dea2964f39bdf74e5d3
Mike <mike@Mikes-Mac-mini.local>
2016-02-11 21:54:38 -0800

    First commit, UIDatePicker example

--- UIDatePicker/ViewController.swift
+++ UIDatePicker/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myDatePicker: UIDatePicker!
+    @IBOutlet weak var selectedDate: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func datePickerAction(sender: AnyObject) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy HH:mm"
+        let strDate = dateFormatter.stringFromDate(myDatePicker.date)
+        self.selectedDate.text = strDate
+    }
 }
 
